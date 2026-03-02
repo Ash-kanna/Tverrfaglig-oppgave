80bcf2124ddb600dbcda11d5ec1a27bf45936b20
ash-kanna <adea.hafnor@gmail.com>
2026-03-02 09:39:18 +0100

    lagde klasser til bruker, produkt, og bestilling

--- demo/myapp/models.py
+++ demo/myapp/models.py
@@ -4,3 +4,17 @@ from django.db import models
 class TodoItem(models.Model):
     title = models.CharField(max_length=200)
     completed = models.BooleanField(default=False)
+
+    
+class Bruker(models.Model):
+    navn = models.CharField(max_length=100)
+    epost = models.EmailField()
+
+class Produkt(models.Model):
+    navn = models.CharField(max_length=100)
+    pris = models.DecimalField(max_digits=10, decimal_places=2)
+
+class Bestilling(models.Model):
+    bruker = models.ForeignKey(Bruker, on_delete=models.CASCADE)
+    produkt = models.ForeignKey(Produkt, on_delete=models.CASCADE)
+    dato = models.DateField()
