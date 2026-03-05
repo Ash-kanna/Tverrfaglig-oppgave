ade082d6e3dcbb1a424d13cf95c3c45eb13b1181
ash-kanna <adea.hafnor@gmail.com>
2026-03-05 10:48:05 +0100

    endret models.py og la til style.css

--- demo/myapp/models.py
+++ demo/myapp/models.py
@@ -1,20 +1,26 @@
 from django.db import models
 
-# Create your models here.
-class TodoItem(models.Model):
-    title = models.CharField(max_length=200)
-    completed = models.BooleanField(default=False)
 
-    
 class Bruker(models.Model):
     navn = models.CharField(max_length=100)
     epost = models.EmailField()
+
+    def __str__(self):
+        return self.navn
+
 
 class Produkt(models.Model):
     navn = models.CharField(max_length=100)
     pris = models.DecimalField(max_digits=10, decimal_places=2)
 
+    def __str__(self):
+        return self.navn
+
+
 class Bestilling(models.Model):
     bruker = models.ForeignKey(Bruker, on_delete=models.CASCADE)
     produkt = models.ForeignKey(Produkt, on_delete=models.CASCADE)
     dato = models.DateField()
+
+    def __str__(self):
+        return f"{self.bruker} - {self.produkt}"
