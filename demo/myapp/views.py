81ea04ded1bc172ea2379c92d7425aa2332ece99
ash-kanna <adea.hafnor@gmail.com>
2026-03-19 10:57:13 +0100

    lagde login knapp som funker og lag medlems knapp

--- demo/myapp/views.py
+++ demo/myapp/views.py
@@ -1,7 +1,22 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 from .models import TodoItem
 
 # Create your views here.
 def home(request):
     return render(request, "home.html")
+
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            messages.success(request, f"Bruker {user.username} ble opprettet. Du kan logge inn nå.")
+            return redirect("login")
+    else:
+        form = UserCreationForm()
+
+    return render(request, "myapp/register.html", {"form": form})
 
